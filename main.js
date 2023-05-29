af57caa999e740bc744471cdca0c2665b07cd0cb
Sebastien LONGO (Klanik) <slongo@klanik.com>
2023-05-29 11:11:59 +0200

    check args and fileExists

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-console.log("slots-protector");
-console.log('arguments: ' + process.argv.slice(2));
+const fs = require('fs');
+
+console.log("slots-protector v" + require('./package.json').version);
+
+if (process.argv.length <= 2) {
+    console.error("missing file argument !!!");
+    process.exit(1);
+}
+
+const inputMizPath = process.argv[2];
+
+if (not(fs.existsSync(inputMizPath))) {
+    console.error("file " + process.argv[2] + " not found !!\nExiting.");
+    process.exit(2);
+}
+
