8c91cc8682711e59080b1e16818a8476431c8e07
Sebastien LONGO (Klanik) <slongo@klanik.com>
2023-05-29 18:09:22 +0200

    add crypto functions

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const fs = require("fs");
 const jszip = require("jszip");
 const path = require("path");
 const { format, parse } = require("lua-json");
+const blake2 = require("blake2");
+const base64url = require('base64url');
 
+if (process.env.NODE_ENV === "production") {
+    console.debug = () => { };
+}
 
 async function mizOpen(mizPath) {
     var MizFile = new jszip();
@@ -13,6 +18,16 @@ async function mizOpen(mizPath) {
 async function getMissionObjectFromZip(zip) {
     let luaTable = 'return { \n' + await zip.file("mission").async("string") + ' }';
     return parse(luaTable).mission;
+}
+
+function generatePassword(length) {
+    let saltKey = new Array(11).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36)[Math.random()<.5?"toString":"toUpperCase"]();});
+    let clearPassword = new Array(length).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36)[Math.random()<.5?"toString":"toUpperCase"]();});
+    let hash = blake2.createKeyedHash('blake2b', Buffer.from(saltKey), {digestLength: 32}).update(Buffer.from(clearPassword)).digest();
+    return {
+        clear: clearPassword,
+        hash: saltKey + ":" + base64url(hash)
+    };
 }
 
 
@@ -32,19 +47,30 @@ if (!(fs.existsSync(inputMizPath))) {
 
 
 mizOpen(inputMizPath).then(mizData => {
-    console.log("mission loaded");
+    console.debug("mission loaded");
     getMissionObjectFromZip(mizData).then(missionObject => {
         Object.keys(missionObject.coalition).forEach(function(coalitionKey, coalitionIndex, coalitionArray) {
             console.debug("coalitions : name = " + missionObject.coalition[coalitionKey].name);
             Object.keys(missionObject.coalition[coalitionKey].country).forEach(function(contryKey, countryIndex, countryArray) {
-                Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group).forEach(function(groupKey, index, array) {
-                    Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units).forEach(function(unitKey, unitIndex, unitArray) {
-                        const unitObject = missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units[unitKey];
-                        if (unitObject.skill === "Client") {
-                            console.log(unitObject);
+                console.debug("country : name = " + missionObject.coalition[coalitionKey].country[contryKey].name);
+                if (Object.hasOwn(missionObject.coalition[coalitionKey].country[contryKey], 'plane')) {
+                    Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group).forEach(function(groupKey, index, array) {
+                        console.debug("Plane group : name = " + missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].name);
+                        const groupObject = missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey];
+                        if (!(Object.hasOwn(groupObject, 'password'))) {
+                            const passwordObject = generatePassword(20);
+                            console.log("group: " + groupObject.name);
+                            console.log("generate Password = " + passwordObject.clear);
+                            console.log("generate hash = " + passwordObject.hash);
+                            Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units).forEach(function(unitKey, unitIndex, unitArray) {
+                                const unitObject = missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units[unitKey];
+                                if (unitObject.skill === "Client") {
+                                    console.debug(groupObject);
+                                }
+                            });
                         }
                     });
-                });
+                }
             });
         });
     });
