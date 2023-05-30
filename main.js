04579a47a86797fecef663e11784d9ab4b60cac8
Sebastien LONGO (Klanik) <slongo@klanik.com>
2023-05-30 10:43:34 +0200

    finalize first version

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+"use strict";
+
 const fs = require("fs");
 const jszip = require("jszip");
 const path = require("path");
 const { format, parse } = require("lua-json");
 const blake2 = require("blake2");
 const base64url = require('base64url');
+const _ = require('lodash');
+const JSZip = require("jszip");
+const slugify = require('slugify');
 
 if (process.env.NODE_ENV === "production") {
     console.debug = () => { };
 }
 
 async function mizOpen(mizPath) {
-    var MizFile = new jszip();
+    let MizFile = new jszip();
     const mizData = fs.readFileSync(mizPath);
     return MizFile.loadAsync(mizData);
 }
@@ -30,6 +35,16 @@ function generatePassword(length) {
     };
 }
 
+function generateGroupInfo(groupObject, credentialObject) {
+    return {
+        groupName: groupObject.name,
+        groupId: groupObject.groupId,
+        force: Object.keys(groupObject.units).length,
+        unitIds: Object.values(groupObject.units).map(x => x.unitId),
+        model: groupObject.units["1"].type,
+        password: credentialObject.clear
+    };
+}
 
 console.log("slots-protector v" + require('./package.json').version);
 
@@ -45,33 +60,58 @@ if (!(fs.existsSync(inputMizPath))) {
     process.exit(2);
 }
 
-
-mizOpen(inputMizPath).then(mizData => {
+const outputMizPath = "./out/passwd_" + path.basename(inputMizPath);
+fs.rmSync("./out", {recursive: true, force: true});
+fs.mkdirSync("./out/coalitions", {recursive: true});
+fs.copyFileSync(inputMizPath,outputMizPath);
+mizOpen(outputMizPath).then(mizData => {
     console.debug("mission loaded");
     getMissionObjectFromZip(mizData).then(missionObject => {
+        mizData.remove("mission");
         Object.keys(missionObject.coalition).forEach(function(coalitionKey, coalitionIndex, coalitionArray) {
+            fs.mkdirSync("./out/coalitions/" + missionObject.coalition[coalitionKey].name);
             console.debug("coalitions : name = " + missionObject.coalition[coalitionKey].name);
             Object.keys(missionObject.coalition[coalitionKey].country).forEach(function(contryKey, countryIndex, countryArray) {
                 console.debug("country : name = " + missionObject.coalition[coalitionKey].country[contryKey].name);
                 if (Object.hasOwn(missionObject.coalition[coalitionKey].country[contryKey], 'plane')) {
                     Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group).forEach(function(groupKey, index, array) {
-                        console.debug("Plane group : name = " + missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].name);
                         const groupObject = missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey];
-                        if (!(Object.hasOwn(groupObject, 'password'))) {
-                            const passwordObject = generatePassword(20);
-                            console.log("group: " + groupObject.name);
-                            console.log("generate Password = " + passwordObject.clear);
-                            console.log("generate hash = " + passwordObject.hash);
-                            Object.keys(missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units).forEach(function(unitKey, unitIndex, unitArray) {
-                                const unitObject = missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units[unitKey];
-                                if (unitObject.skill === "Client") {
-                                    console.debug(groupObject);
-                                }
-                            });
+                        console.debug("Plane group : name = " + missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].name);
+                        const listSkills = Object.values(missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].units).map(x => x.skill).find(element => element === "Client");
+                        if (typeof listSkills !== 'undefined' && listSkills) {
+                            if (!(Object.hasOwn(groupObject, 'password'))) {
+                                const passwordObject = generatePassword(20);
+                                fs.writeFileSync("./out/coalitions/" + missionObject.coalition[coalitionKey].name + "/" + slugify(groupObject.name) + "-info.json", JSON.stringify(generateGroupInfo(groupObject, passwordObject), null, "  "));
+                                console.debug("group: " + groupObject.name);
+                                console.debug("generate Password = " + passwordObject.clear);
+                                console.debug("generate hash = " + passwordObject.hash);
+                                missionObject.coalition[coalitionKey].country[contryKey].plane.group[groupKey].password=passwordObject.hash;
+                            }
+                        } else {
+                            console.debug("not Human slot");
                         }
                     });
                 }
             });
         });
+        let missionLuaT = format(missionObject, { singleQuote: false })
+        missionLuaT = missionLuaT
+            .split('\n')
+            .slice(1, -1)
+            .join('\n')
+            .slice(0, -1)
+            .replace(/\[\"(\d+)\"\] = /g, "[$1] = ");
+        mizData.file("mission", missionLuaT);
+        mizData.generateAsync({
+            type: 'nodebuffer',
+            streamFiles: true,
+            compression: "DEFLATE",
+            compressionOptions: {
+                level: 9
+            }
+        }).then(inputZip => {
+            fs.writeFileSync(outputMizPath, inputZip);
+        });
     });
-});
+})
+
